3269f57c7af620b1355b0650a669fc4a41368115
Bellidojoan <joanavarrobellido@gmail.com>
2020-09-29 20:55:51 +0200

    Update

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,5 +14,14 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products {get; set; }
+        public DbSet<ProductBrand> ProductsBrands {get; set; }
+        public DbSet<ProductType> ProductsTypes {get; set; }
+        public object ProductTypes { get; internal set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
